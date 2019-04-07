94774b6caaeeb24d2f5c80d04617c474a724d951
yFGq-Ziw <rui_aso@live.jp>
2019-04-07 21:49:42 +0000

    follow, unfollow

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,23 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  include SessionsHelper
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
+  end
+
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
+end
