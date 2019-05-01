3ad1e7bc224792d9916fb45cedda53afe859943c
yFGq-Ziw <rui_aso@live.jp>
2019-05-01 14:23:37 +0000

    onegai2

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,9 +1,7 @@
 class RankingsController < ApplicationController
   def follow
     @co = Fobitow.group(:category).order('count_category desc').count(:category)
-    @user = User.all.search(params[:search])
     @ranking_counts = Follow.ranking
-    @kazu = @user.count
     @users = User.all.order('@ranking_counts desc').search(params[:search]).page(params[:page]).per(24)
   end
 end
