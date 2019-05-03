c64fce38b0b9d3234bc145434bf5f6b7cb651014
yFGq-Ziw <rui_aso@live.jp>
2019-05-03 06:44:13 +0000

    acts-as-taggable-on

--- vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.3/ext/nokogiri/tmp/x86_64-pc-linux-gnu/ports/libxml2/2.9.9/libxml2-2.9.9/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/ec2-user/environment/fobitows/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.3/ports/x86_64-pc-linux-gnu/libxml2/2.9.9/lib"
-XML2_LIBS="-lxml2 -lz       -lm "
-XML2_INCLUDEDIR="-I/home/ec2-user/environment/fobitows/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.3/ports/x86_64-pc-linux-gnu/libxml2/2.9.9/include/libxml2"
-MODULE_VERSION="xml2-2.9.9"
-
--- vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.3/ext/nokogiri/tmp/x86_64-pc-linux-gnu/ports/libxslt/1.1.33/libxslt-1.1.33/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/home/ec2-user/environment/fobitows/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.3/ports/x86_64-pc-linux-gnu/libxslt/1.1.33/lib"
-XSLT_LIBS="-lxslt -L/home/ec2-user/environment/fobitows/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.3/ports/x86_64-pc-linux-gnu/libxml2/2.9.9/lib -lxml2 -lz -lm -ldl -lm "
-XSLT_INCLUDEDIR="-I/home/ec2-user/environment/fobitows/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.3/ports/x86_64-pc-linux-gnu/libxslt/1.1.33/include"
-MODULE_VERSION="xslt-1.1.33"
